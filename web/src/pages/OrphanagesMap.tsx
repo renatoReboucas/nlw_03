80e1095bdcc3c58ed70ef37ae4cbb0e0f8ad186b
RenatoReboucas <renato190698@gmail.com>
2020-10-12 14:18:54 -0300

    corrigindo map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,43 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import { FiPlus } from 'react-icons/fi'
-
-//mapa
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import { Map, TileLayer } from 'react-leaflet';
+
 import 'leaflet/dist/leaflet.css';
 
-import '../styles/pages/orphanages-map.css'
-import mapMarkerImg from '../assets/map.marker.svg'
+import mapMarkerImg from '../assets/map-marker.svg';
 
-export default function OrphanagesMap() {
-  // console.log(process.env.REACT_APP_MAPBOX_TOKEN);
-  
+import '../styles/pages/orphanages-map.css';
+
+const OrphanagesMap: React.FC = () => {
   return (
     <div id="page-map">
       <aside>
         <header>
-          <img src={mapMarkerImg} alt="Happy"/>
+          <img src={mapMarkerImg} alt="Happy" />
 
           <h2>Escolha um orfanato no mapa</h2>
           <p>Muitas crianças estão esperando a sua visita :)</p>
         </header>
+
         <footer>
-          <strong>São Paulo</strong>
-          <span>São Paulo</span>
+          <strong>Brasília</strong>
+          <span>Distrito Federal</span>
         </footer>
       </aside>
-      
+
       <Map
-        center={[23.6990438,-46.6529385]}
+        center={[-15.7966153, -47.8770008]}
         zoom={15}
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
       </Map>
 
-      <Link to="" className="create-orphanages" >
+      <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
-  )
+  );
 }
+
+export default OrphanagesMap;
